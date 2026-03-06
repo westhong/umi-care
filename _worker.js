cd226e92216498660030e4c3cd28e02add3a7b47
West Wong <westhong@yahoo.com>
2026-03-06 11:20:34 -0700

    fix(v3.3): version bump to 3.3

--- _worker.js
+++ _worker.js
@@ -63,7 +63,7 @@ async function handleApi(request, env, url) {
 
   try {
     // PING
-    if (path === '/ping') return json({ ok: true, version: '3.2', kv: !!KV });
+    if (path === '/ping') return json({ ok: true, version: '3.3', kv: !!KV });
 
     // PIN
     if (path === '/pin/check') {
